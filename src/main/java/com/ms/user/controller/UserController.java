34e1b6bd85609a34a11140621486f87d56b64d45
sumit pawar <sumit.pawar@digital.datamatics.com>
2025-07-03 15:41:46 +0530

    delete added

--- src/main/java/com/ms/user/controller/UserController.java
+++ src/main/java/com/ms/user/controller/UserController.java
@@ -31,5 +31,11 @@ public class UserController {
         return userservice.getAllUsers();
     }
 
+    //delte bu id
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable Integer id) {
+        userservice.deleteUserById(id);
+    }
+
 }
 
